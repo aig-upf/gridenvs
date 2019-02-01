f1e012fe49d0b43e69cf55de72fde4e7be3fb854
dallonsi <damien.allonsius@univ-amu.fr>
2019-02-01 12:47:14 +0100

    forgot to add keyboard_agent.py

---
+++ keyboard_agent.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from gridenvs.keyboard_controller import Controls, KeyboardController
+
+def run_env(env_name, controls={**Controls.Arrows, **Controls.KeyPad}, frameskip=1, obs_fn = lambda x:None):
+    import gym
+    env = gym.make(env_name)
+
+    if not hasattr(env.action_space, 'n'):
+        raise Exception('Keyboard agent only supports discrete action spaces')
+
+    controller = KeyboardController(env, controls=controls, frameskip=frameskip, obs_fn=obs_fn)
+    try:
+        controller.run()
+    finally:
+        env.close()
+
+if __name__ == "__main__":
+    import sys
+    import gridenvs.examples  # load example gridworld environments
+
+    env_name = 'GE_MazeKeyDoor-v4' if len(sys.argv)<2 else sys.argv[1] #default environment or input from command line
+    run_env(env_name)
