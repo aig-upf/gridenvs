225a37706ee6c498d92c4851348ce7bb3958c3be
dallonsi <damien.allonsius@univ-amu.fr>
2019-02-28 11:35:00 +0100

    some typos on kwargs

--- gridenvs/examples/__init__.py
+++ gridenvs/examples/__init__.py
@@ -67,5 +67,5 @@ register(id='GE_MazeOptions-v0',
 
 register(id='GE_MazeOptions-v1',
              entry_point='gridenvs.examples.maze_options:key_door_walls',
-             kwargs={zone_size_x : 2 , zone_size_y : 2, blurred = True},
+             kwargs={'zone_size_x' : 2 , 'zone_size_y' : 2, 'blurred' : True},
              nondeterministic=False)
