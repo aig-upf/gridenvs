36f737e56fe71c8ba22fd3882ee321fdf03d9b8d
dallonsi <damien.allonsius@univ-amu.fr>
2019-07-17 23:23:41 +0200

    added a setup file so that gridenvs can now be installed as a library

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gridenvs',
+    version='0.1',
+    keywords='environment, agent, rl, openaigym, openai-gym, gym',
+    install_requires=[
+        'gym>=0.10.9',
+        'numpy>=1.15.4',
+        'opencv-python>=3.4.5.20'
+    ]
+)
